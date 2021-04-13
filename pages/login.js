eb70fa6cf86e4a51ecd6eefa06d42dce4a740f04
JhsK <qwe6293@nate.com>
2021-04-13 22:15:06 +0900

    [Refactor] 코드 위치 수정

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import LoginLayout from "../components/LoginLayout";
 
 const login = () => {
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
-  return (
-    <>
-      {isLoggedIn ? <LoginLayout /> : alert("로그인을 통해 접근이 가능합니다")}
-    </>
-  );
+  return <LoginLayout />;
 };
 
 export default login;
--- pages/main.js
+++ pages/main.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import MainLayout from "../components/MainLayout";
 
 const main = () => {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   return (
     <>
-      <MainLayout />
+      {isLoggedIn ? <MainLayout /> : alert("로그인을 통해 접근이 가능합니다")}
     </>
   );
 };
